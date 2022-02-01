03305de255078366cc59a077d629ef02a3bbff0e
sper0054 <teresa.sperl@gmail.com>
2022-02-01 14:54:52 -0600

    intern and eningeer

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -1,7 +1,19 @@
 const Engineer = require ('../lib/Engineer.js');
 
-//github//github username
+test("Can set GitHub username", () => {
+    const testValue = "GitHubusername";
+    const e = new Engineer("Dave", 12345, "test@test.com", testValue);
+    expect(e.github).toBe(testValue);
+});
 
-//get GitHub()
+test("getRole() to be engineer", () => {
+    const testValue = "Engineer";
+    const e = new Engineer("Dave", 12345, "test@test.com", "GitHubusername");
+    expect(e.getRole()).toBe(testValue);
+});
 
-//getRole() // overridden to retun "Engineer"
+test("Can get GitHub username via getGithub()", () => {
+    const testValue = "GitHubusername";
+    const e = new Engineer("Dave", 12345, "test@test.com", testValue);
+    expect(e.getGithub()).toBe(testValue);
+});
--- __tests__/intern.test.js
+++ __tests__/intern.test.js
@@ -1,7 +1,19 @@
 const Intern = require ('../lib/Intern.js');
 
-//School
+test("Can set intern school", () => {
+    const testValue = "Michigan St";
+    const e = new Intern("Dave", 12345, "test@test.com", testValue);
+    expect(e.school).toBe(testValue);
+});
 
-//getSchool()
+test("getRole() to be intern", () => {
+    const testValue = "Intern";
+    const e = new Intern("Dave", 12345, "test@test.com", "Michigan St");
+    expect(e.getRole()).toBe(testValue);
+});
 
-//get Role() //overridden to return 'intern'
+test("Can get school via getSchool()", () => {
+    const testValue = "Michigan St";
+    const e = new Intern("Dave", 12345, "test@test.com", testValue);
+    expect(e.getSchool()).toBe(testValue);
+});
