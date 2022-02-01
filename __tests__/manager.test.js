b2a77871efc4f5ae40542998867490f9bbc92f86
sper0054 <teresa.sperl@gmail.com>
2022-02-01 14:58:14 -0600

    manager

--- __tests__/manager.test.js
+++ __tests__/manager.test.js
@@ -1,5 +1,13 @@
 const Manager = require ('../lib/Manager.js');
 
-//officeNumber
+test("Can set manager office number", () => {
+    const testValue = 100;
+    const e = new Manager("Dave", 12345, "test@test.com", testValue);
+    expect(e.officeNumber).toBe(testValue);
+});
 
-//getRole() // Overridden to return 'Manager'
+test("getRole() to be manager", () => {
+    const testValue = "Manager";
+    const e = new Manager("Dave", 12345, "test@test.com", 100);
+    expect(e.getRole()).toBe(testValue);
+});
